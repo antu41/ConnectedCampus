e8dcec5a54d6f74bf434f3e4e6688a9923447909
Antu Das <Antu Das@ICARUS>
2023-12-15 23:27:21 +0600

    Add project files.

---
+++ ConnectedCampusWeb/Models/Student.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ConnectedCampusWeb.Models
+{
+    public class Student
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public int name { get; set; }
+        [Required]
+        public int Class { get; set; }
+    }
+}
---
+++ ConnectedCampusWeb/Models/Teacher.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ConnectedCampusWeb.Models
+{
+    public class Teacher
+    {
+        [Key]
+        public int id { get; set; }
+        [Required]
+        public string name { get; set; }
+        [Required]
+        public string description { get; set; }
+
+    }
+}
